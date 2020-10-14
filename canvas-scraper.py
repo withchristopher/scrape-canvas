d38b62a378d3e81e5bbf8b03d6a0dbc3b1b900f0
Chris Maxwell <chmaxwel@icloud.com>
2020-10-14 16:49:07 +0200

    Update canvas-scraper.py

--- canvas-scraper.py
+++ canvas-scraper.py
@@ -18,10 +18,12 @@ def extract_files(text):
 
 def get_course_files(course):
     modules = course.get_modules()
-
+    
+    new_modules = modules[20:25]
+                     
     files_downloaded = set() # Track downloaded files for this course to avoid duplicates
 
-    for module in modules:
+    for module in new_modules:
         module: Module = module
         module_items = module.get_module_items()
         for item in module_items:
